b1feb77a58dbab9c01e79b8a9ff8e97dab47e41a
Ogunyebi Ayokanmi <ogunyebiayokanmi@gmail.com>
2025-12-13 16:04:43 +0100

    fix issue

--- app/(client)/category/[category]/page.tsx
+++ app/(client)/category/[category]/page.tsx
@@ -1,10 +1,13 @@
 import ProductsPageX from "@/components/features/products/ProductsPageX"
 import { fetchCategories } from "@/lib/api"
 
-export default async function CategoryPage({ params }: { params: { category: string } }) {
-  const { category } = params
-  const categories = await fetchCategories()
-
+export default async function CategoryPage({ 
+  params 
+}: { 
+  params: Promise<{ category: string }> 
+}) {
+  const { category } = await params;
+  const categories = await fetchCategories();
   if (!categories) {
     return (
       <div className="p-10 text-center text-xl text-gray-500">
